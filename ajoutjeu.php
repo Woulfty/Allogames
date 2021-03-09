34958a7dcccc6145ba40f2fdb9ce14e55b0181f4
AlexandreCare <70692837+AlexandreCare@users.noreply.github.com>
2021-03-09 15:19:30 +0100

    Add files via upload

--- ajoutjeu.php
+++ ajoutjeu.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    include "fonction.php";
-    include "Objetuser.php";
+    include "fonction.php"; //appel de la page de fonction 
+    include "Objetuser.php"; //appel des objets user
 
     if(check()){
         header("Location: index.php");
@@ -48,30 +48,41 @@
                                             <?php
                                                 if (isset($_POST["subModif"])){
                                                     $note = $MaBase->query("INSERT INTO Game (`nom`,`texte`,`type`) VALUES (\"".$_POST['nom']."\",\"".$_POST['dudule']."\",\"".$_POST['type']."\")");
+                                                    //insertion dans la base Game du nom de la bio et du type
                                                     if($note){
+                                                        //si le jeu est bien ajouté message positif
                                                         echo"jeu ajouté";
                                                     }else{
+                                                        //si le jeu n'est pas ajouté message négatif
                                                         echo"une erreur est survenu";
                                                     }
-                                                
+                                                    //type d'imge valide/autorisé 
                                                     $valideType = array('.jpg');
                                                     if ($_FILES['Affi'] == 0) {
+                                                        //si il n'y a pas d'image, message d'erreur
                                                         echo "aucun dossier selectionné";
                                                         die;
                                                     }
+
                                                     $fileType = ".".strtolower(substr(strrchr($_FILES['Affi']["name"], '.'), 1));
+                                                    //selection de l'id du dernier jeu ajouté
                                                     $req = $MaBase->query("SELECT id FROM Game ORDER BY id DESC LIMIT 1")->fetch();
-                                                
+                                                    //récupération du nom de l'image et modification avec l'id du dernier jeu pour l'affiche
                                                     $_FILES['Affi']["name"] = $req["id"]."_Affiche";
                                                     if (!in_array($fileType, $valideType)) {
+                                                        // si l'image ne respécte pas le fileType et le valideType message d'erreur
                                                         echo "le fichier sélectionné n'est pas une image";
                                                         die;
                                                     }
+                                                    //récupération du nouveau nom de l'image
                                                     $tmpName = $_FILES['Affi']['tmp_name'];
                                                     $Name = $_FILES['Affi']['name'];
+                                                    //séléction du fichier ou l'image sera insérer 
                                                     $fileName = "IMG/Games/" . $Name.".jpg";
+                                                    //déplacement de l'image dans le dossier
                                                     $résultUplod = move_uploaded_file($tmpName, $fileName);
                                                     if ($résultUplod) {
+                                                        //si l'image est déplacer message positif 
                                                         echo "Affiche enregister ";
                                                     }
                                                     //Background
@@ -80,17 +91,22 @@
                                                         die;
                                                     }
                                                     $fileType = ".".strtolower(substr(strrchr($_FILES['back']["name"], '.'), 1));
-                                                
+                                                    //récupération du nom de l'image et modification avec l'id du dernier jeu pour le background
                                                     $_FILES['back']["name"] = $req["id"]."_background";
                                                     if (!in_array($fileType, $valideType)) {
+                                                        // si l'image ne respécte pas le fileType et le valideType message d'erreur
                                                         echo "le fichier sélectionné n'est pas une image";
                                                         die;
                                                     }
+                                                    //récupération du nouveau nom de l'image
                                                     $tmpName = $_FILES['back']['tmp_name'];
                                                     $Name = $_FILES['back']['name'];
+                                                    //séléction du fichier ou l'image sera insérer 
                                                     $fileName = "IMG/Games/" . $Name.".jpg";
+                                                    //déplacement de l'image dans le dossier
                                                     $résultUplod = move_uploaded_file($tmpName, $fileName);
                                                     if ($résultUplod) {
+                                                        //si l'image est déplacer message positif
                                                         echo "Background enregister";
                                                     }
                                                 
@@ -105,8 +121,9 @@
                                             <h2 class="gris">Modifier/supprimer un jeu :</h2>
                                             <select name="remove">
                                                 <?php $jeux = $MaBase->query("SELECT nom, id FROM Game"); 
-
+                                                    //séléction du non et de l'id dans la base Game
                                                     while ($jeu = $jeux->fetch())
+                                                        //boucle qui affiche l'id et le nom du jeu
                                                         echo "<option value=".$jeu["id"].">".$jeu["nom"]."</option>";
                                                 ?>
 
@@ -119,9 +136,11 @@
                                         <form enctype="multipart/form-data" method="post" class="form1">
                                             <?php
                                                 if (isset($_POST["update"])){ 
+                                                    //si le bouton "update" est cliqué
                                                     $gameData = $MaBase->query("SELECT * FROM Game WHERE id = ".$_POST["remove"])->fetch();
+                                                    //séléction dans jeu via l'id du jeu choisi
                                                 ?> 
-                                
+                                                    <!--Affichage du nom choisie-->
                                                     <h2>Modification de <?= $gameData["nom"]; ?></h2>
                                 
                                                     <p>ID du jeu :</p>
@@ -146,6 +165,7 @@
                                                         <!--ajout d'une image-->
                                                         <input class="input" type="file" name='Affi'/>
                                                         <p>
+                                                            <!--Affiche du jeu séléctionné-->
                                                             <a href="<?= "IMG/Games/".$_POST["remove"]."_Affiche.jpg"?>">
                                                                 <img src="<?= "IMG/Games/".$_POST["remove"]."_Affiche.jpg"?>" alt="" width="160px">
                                                             </a>
@@ -158,26 +178,28 @@
                                                         <!--ajout d'une image-->
                                                         <input class="input" type="file" name='back'/>
                                                         <p>
+                                                            <!--Background du jeu séléctionné-->
                                                             <a href="<?= "IMG/Games/".$_POST["remove"]."_background.jpg"?>">
                                                                 <img src="<?= "IMG/Games/".$_POST["remove"]."_background.jpg"?>" alt="" width="300px">
                                                             </a>
                                                         </p>
                                                     </p>
                                                     <p>
+                                                        <!--Bouton pour enregistrer les modifications-->
                                                         <input class="input" type="submit" name='sendEdit' value="Enregistrer les modifications">
                                                     </p>
                                                 <?php 
                                                 }
-
+                                                //si le bouton "sendedit" est clické
                                                 if (isset($_POST["sendEdit"])) {
-                
+                                                    //Upadate dans la base game du nom, du type, de la bio du jeu modifier
                                                     $stm = $MaBase->prepare("UPDATE Game SET `nom` = ?, `type` = ?, `texte` = ? WHERE id = ".$_POST["id"]);
                                                     $stm->execute(array(
                                                         $_POST["nom"],
                                                         $_POST["type"],
                                                         $_POST["dudule"],
                                                     ));
-                                    
+                                                    //type d'image valide
                                                     $valideType = array('.jpg');
                                                     
                                                     if (!$_FILES['Affi']["name"]) die;
@@ -185,7 +207,7 @@
                                                     echo "pass";
                                     
                                                     $fileType = ".".strtolower(substr(strrchr($_FILES['Affi']["name"], '.'), 1));
-                                    
+                                                    //séléctionne le nom de l'image et le modifie avec l'id du jeu pour l'image
                                                     $_FILES['Affi']["name"] = $_POST["id"]."_Affiche";
                                                     
                                                     if (!in_array($fileType, $valideType)) {
@@ -195,18 +217,20 @@
                                     
                                                     $tmpName = $_FILES['Affi']['tmp_name'];
                                                     $Name = $_FILES['Affi']['name'];
+                                                    //séléction du dossier dans lequel mettre l'image
                                                     $fileName = "IMG/Games/" . $Name.".jpg";
+                                                    //déplacement de l'image
                                                     $résultUplod = move_uploaded_file($tmpName, $fileName);
                                                     if ($résultUplod) {
                                                         echo "Affiche enregister ";
                                                     }
-                                                    //Background
+                                                    //si il y a aucun fichier séléctionné 
                                                     if ($_FILES['back'] == 0) {
                                                         echo "aucun dossier selectionné";
                                                         die;
                                                     }
                                                     $fileType = ".".strtolower(substr(strrchr($_FILES['back']["name"], '.'), 1));
-                                    
+                                                    //séléctionne le nom de l'image et le modifie avec l'id du jeu pour le background
                                                     $_FILES['back']["name"] = $_POST["id"]."_background";
                                                     
                                                     if (!in_array($fileType, $valideType)) {
@@ -215,29 +239,35 @@
                                                     }
                                                     $tmpName = $_FILES['back']['tmp_name'];
                                                     $Name = $_FILES['back']['name'];
+                                                    //séléction du dossier dans lequel mettre l'image
                                                     $fileName = "IMG/Games/" . $Name.".jpg";
+                                                    //déplacement de l'image
                                                     $résultUplod = move_uploaded_file($tmpName, $fileName);
                                                     if ($résultUplod) {
                                                         echo "Background enregister";
                                                     }
                                     
                                                 }
-                                    
+                                                //si le bouton "send" est cliqué
                                                 if (isset($_POST["send"])){
-                                    
+                                                    
                                                     $id = $_POST["remove"]; 
+                                                    //suppression des commentaires de ce jeu
                                                     $MaBase->query("DELETE FROM commentaires WHERE idjeux = ".$id);
+                                                    //supression des LIKES de ce jeu
                                                     $MaBase->query("DELETE FROM ArticleLike WHERE IDJeu = ".$id);
+                                                    //supression du jeu
                                                     $MaBase->query("DELETE FROM Game WHERE id = ".$id);
-                                    
+                                                    
                                                     $affiche    = "IMG/Games/".$id."_Affiche.jpg";
+                                                    
                                                     $background = "IMG/Games/".$id."_background.jpg";
-                                    
+                                                    //suppression de l'affiche du jeu
                                                     if (file_exists($affiche)) {
                                                         unlink($affiche);
                                                         echo "L'affiche a bien été supprimée";
                                                     } else echo "L'affiche n'a pas été supprimée";
-                                    
+                                                    //suppression du background de ce jeu
                                                     if (file_exists($background)) {
                                                         unlink($background);
                                                         echo "Le background a bien été supprimée";
--- compte.php
+++ compte.php
@@ -28,27 +28,87 @@
                     <p>
                         <?php
                             $utilisateurs = $MaBase->query("SELECT * FROM `User` WHERE `id`='".$_SESSION["idUser"]."'");
+                            //séléction des données de l'utilisateur via son id
                             $utilisateurs = $utilisateurs->fetch();
-
-                            echo '<h2>'.$utilisateurs['nom'].'</h2>';
+                            //affiche le pseudo de l'utilisateur
+                            echo '<h2>'.$utilisateurs['pseudo'].'</h2>';
                         ?>
                     </p>
                     <?php
-
+                        //création d'un objet user
                         $user = new User($MaBase, $_SESSION["idUser"]);
-
+                        //fonction pour l'image de profil
                         $user->PDP();
-
+                        //fonction pour la sécurité
                         $user->sécu();
 
                     ?>
+                    <script type="text/javascript" src="Js/js1.js"></script>
+                        <style>
+                            .espace{
+                                height: 3%;
+                            }
+                            .espace2{
+                                height: 10%;
+                                width: 100%;
+                            }   
+                            .center{
+                                text-aling:center;
+                                position: revert;
+                                margin: auto;
+                            }
+                            .popup {
+                                position: fixed;
+                                top: 230px;
+                                left: 27%;
+                                height: 180px;
+                                width: 50%;
+                                display: flex;
+    
+                                justify-content: center;
+                                text-align: center;
+                                flex-direction: column;
+                                transition: all .35s;
+                                border: 3.3px solid #000000;
+                                display: none;
+                                background-color: #888484;
+                            }
+                            #newe {
+                                position: sticky;
+                                background-color: white;
+                                text-align: left;
+                                width: 60%;
+                                padding: 50px;
+                                transition: all .35s;
+                            }
+                        </style>
+                        <?php
+                            if ( $utilisateurs['pdp'] == "default.png") { 
+                                ?>  
+                                    <div class="popup">
+                                        <h3><p>Tu sais, tu peux changer de photo de profil, tu seras vraiment unique ! </p></h3>
+                                        <input class="poster" type="button" value="OK !" id="ModalRemove">
+                                    </div>
+                                <?php 
+                            } 
+                        ?>
+                        <script>
+                            const modalRemove = document.getElementById("ModalRemove")
+                            const modal = document.querySelector(".popup")
+                    
+                            setTimeout(() => {
+                                modal.style.display = "flex"
+                            }, 3000);
+
+                            modalRemove.addEventListener("click", () => {
+                            modal.style.opacity = "0";
+                            setTimeout(() => modal.style.display = "none ", 350)
+                            })
+                        </script>
                 </form>
             </div>
         </div>
     </div>
-
-    <!--mettre le script js pour l'image de profil  (default)-->
-
     <?php
     }
     ?>
